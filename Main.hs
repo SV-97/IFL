721a9c54f571d2d0cc0de669da5816db2665943d
Stefan Volz <volzstefan97@googlemail.com>
2020-03-20 00:12:55 +0100

    Chapter 1.5.4

--- Main.hs
+++ Main.hs
@@ -61,6 +61,7 @@ preludeDefs =
       , EAp (EAp (EVar "f") (EVar "x")) (EAp (EVar "g") (EVar "x")))
     , ("compose", ["f", "g", "x"], EAp (EVar "f") (EAp (EVar "g") (EVar "x")))
     , ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f"))
+    , ("testId", [], ELam ["x"] (EVar "x"))
     ]
 
 class (Monoid iseq, IsString iseq) =>
@@ -81,11 +82,44 @@ class (Monoid iseq, IsString iseq) =>
   iInterleave s ss = iConcat $ intersperse s ss
 
 pprint :: ISeq iseq => CoreProgram -> iseq
-pprint e = ""
+pprint = pprSupercombs
   where
+    combine :: ISeq iseq => (a -> iseq) -> [a] -> iseq
+    combine f as = iInterleave sep (map f as)
+      where
+        sep = iConcat [";", iNewline]
+    pprSupercombs :: ISeq iseq => [SuperCombDef Name] -> iseq
+    pprSupercombs = combine pprSupercomb
+    pprSupercomb :: ISeq iseq => SuperCombDef Name -> iseq
+    pprSupercomb (SCDef (name, args, expr)) =
+      iConcat
+        [ iInterleave " " (iStr name : map iStr args)
+        , " = "
+        , iIndent (pprExpr expr)
+        ]
+    pprDefs :: ISeq iseq => [(Name, CoreExpr)] -> iseq
+    pprDefs = combine pprDef
+    pprDef :: ISeq iseq => (Name, CoreExpr) -> iseq
+    pprDef (name, expr) = iConcat [iStr name, " = ", iIndent (pprExpr expr)]
+    pprAExpr e
+      | isAtomicExpr e = pprExpr e
+      | otherwise = "(" <> pprExpr e <> ")"
+    pprAlt :: ISeq iseq => CoreAlt -> iseq
+    pprAlt (Alter (num, vars, body)) =
+      iConcat
+        [ "<"
+        , iStr $ show num
+        , "> "
+        , iInterleave " " (map iStr vars)
+        , " -> "
+        , pprExpr body
+        ]
+    pprAlts :: ISeq iseq => [CoreAlt] -> iseq
+    pprAlts = combine pprAlt
+    pprExpr :: ISeq iseq => CoreExpr -> iseq
     pprExpr (ENum n) = iStr $ show n
     pprExpr (EVar v) = iStr v
-    pprExpr (EAp e1 e2) = pprExpr e1 <> " " <> pprExpr e2
+    pprExpr (EAp e1 e2) = pprExpr e1 <> " " <> pprAExpr e2
     pprExpr (ELet isrec defs expr) =
       iConcat
         [ iStr keyword
@@ -101,18 +135,18 @@ pprint e = ""
           if isrec
             then "letrec"
             else "let"
-    pprDefs defs = iInterleave sep (map pprDef defs)
-      where
-        sep = iConcat [";", iNewline]
-    pprDef (name, expr) = iConcat [iStr name, " = ", iIndent (pprExpr expr)]
-    pprAExpr e
-      | isAtomicExpr e = pprExpr e
-      | otherwise = "(" <> pprExpr e <> ")"
+    pprExpr (ECase value alternatives) =
+      iConcat ["case ", pprAExpr value, " of", iNewline, pprAlts alternatives]
+    pprExpr (ELam args body) =
+      iConcat ["λ", iInterleave " " (map iStr args), ". ", pprExpr body]
 
 data ISeqRep
   = INil
   | IStr String
   | IAppend ISeqRep ISeqRep
+  | IIndent ISeqRep
+  | INewline
+  deriving (Show)
 
 instance Semigroup ISeqRep where
   INil <> b = b
@@ -125,17 +159,24 @@ instance Monoid ISeqRep where
 instance IsString ISeqRep where
   fromString = IStr
 
-flatten :: [ISeqRep] -> String
-flatten []                       = ""
-flatten (INil:seqs)              = flatten seqs
-flatten (IStr s:seqs)            = s ++ flatten seqs
-flatten (IAppend seq1 seq2:seqs) = flatten (seq1 : seq2 : seqs)
+flatten :: Int -> [(ISeqRep, Int)] -> String
+flatten _ [] = ""
+flatten col ((INil, _):seqs) = flatten col seqs
+flatten col ((IStr s, _):seqs) = s ++ flatten col seqs
+flatten col ((IAppend seq1 seq2, indent):seqs) =
+  flatten col ((seq1, indent) : (seq2, indent) : seqs)
+flatten col ((INewline, indent):seqs) =
+  "\n" ++ spaces indent ++ flatten indent seqs
+  where
+    spaces n = take n $ cycle " "
+flatten col ((IIndent seq, indent):seqs) = flatten col ((seq, col) : seqs)
 
 instance ISeq ISeqRep where
-  iIndent = id
-  iNewline = "\n"
-  iDisplay seq = flatten [seq]
+  iIndent = IIndent
+  iNewline = INewline
+  iDisplay seq = flatten 0 [(seq, 0)]
 
 main :: IO ()
 main = do
-  print "crack"
+  print "hey"
+  putStrLn . iDisplay $ (pprint preludeDefs :: ISeqRep)
